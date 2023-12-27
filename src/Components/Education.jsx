9785292519410271a73992e4730f2b22fcec7ec8
Victor Ariel <70303828+Victor2411-hub@users.noreply.github.com>
2023-12-27 19:40:28 +0000

    a

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -4,6 +4,28 @@ const Education = () => {
     return(
         <section className="light" id="education">
             <h2>Education</h2>
+            <div className="grid">
+                <div className="cell">
+                    <h3>Politécnico Braulio Paulino</h3>
+                    <p>High School</p>
+                    <p>2017-2020</p>
+                </div>
+                <div className="cell">
+                    <h3>Politécnico Braulio Paulino</h3>
+                    <p>Software Development</p>
+                    <p>2020-2023</p>
+                </div>
+                <div className="cell">
+                    <h3>Microsoft Learn Student Ambassadors</h3>
+                    <p>MLSA Onboarding</p>
+                    <p>2021</p>
+                </div>
+                <div className="cell">
+                    <h3>Microsoft Learn Student Ambassadors</h3>
+                    <p>MLSA Onboarding</p>
+                    <p>2021</p>
+                </div>
+            </div>
         </section>
     )
 }
