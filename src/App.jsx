66bdbf464fc6872391506d63a6ca343cf9b9f884
Victor Ariel <70303828+Victor2411-hub@users.noreply.github.com>
2023-12-27 18:08:53 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Victor De León Hernández",
+  title: "Web Designer & Developer",
+  email: "victorpbpcontacto@gmail.com",
+  gitHub: "https://github.com/Victor2411-hub",
+  instagram: "https://www.instagram.com/iam._victorad/",
+  linkedIn: "https://www.linkedin.com/in/v%C3%ADctor-ariel-de-le%C3%B3n-hern%C3%A1ndez-796399201/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/desk2.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "desk with laptop, notebook, and coffee";
 
 /**
  * Project list
@@ -31,28 +31,21 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "School Website",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Created a website for my school, Polythecnic Braulio Paulino, to help students find information about the school and its programs.",
+    url: "https://www.politecnicobrauliopaulino.edu.do",
   },
   {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
+    title: "Sweepstakes system",
+    description: "Created a sweepstakes system for my school.",
     url: "https://github.com/microsoft/web-dev-for-beginners",
   },
   {
     title: "My Resume Site",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    url: "https://github.com/Victor2411-hub/MLSA-Onboarding",
   },
 ];
 
