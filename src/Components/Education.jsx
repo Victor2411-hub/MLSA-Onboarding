59f0f4beaf40f21856d6cdfc4bec92708511b6ac
Victor Ariel <70303828+Victor2411-hub@users.noreply.github.com>
2023-12-27 19:03:01 +0000

    a

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,22 +1,11 @@
 import React from "react";
 
 const Education = () => {
-    return (
-        <section className="padding" id="education">
-        <h2 style={{ textAlign: "center" }}>Education</h2>
-        <div className="container">
-            <div className="row">
-            <div className="col-3">
-                <h3>2019 - Present</h3>
-            </div>
-            <div className="col-9">
-                <h3>Politecnic Braulio Paulino</h3>
-                <p>Software Development</p>
-            </div>
-            </div>
-        </div>
+    return(
+        <section className="light" id="education">
+            <h2>Education</h2>
         </section>
-    );
-    };
+    )
+}
 
 export default Education;
